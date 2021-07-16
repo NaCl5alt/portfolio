bedeaabbadd769fd6fa4cf6b95990e0c42cde3a4
S.S <s21020@g.metro-cit.ac.jp>
2021-07-17 01:53:12 +0900

    Change base of vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
   define: { "process.env": {} },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
