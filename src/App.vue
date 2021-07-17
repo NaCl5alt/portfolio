b91149cce1263ec653fe2f61f40a2899bf06ccfe
S.S <s21020@g.metro-cit.ac.jp>
2021-07-17 16:18:42 +0900

    downgrade vue 3 to 2

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
     <v-card class="overflow-hidden">
       <v-app-bar absolute color="white" elevate-on-scroll scroll-target="#main">
         <v-spacer />
-        <ul class="v-breadcrumbs v-breadcrumbs--density-default">
-          <template v-for="item in items" :key="item.text">
-            <a v-bind:href="item.href">
-              {{ item.text }}
-            </a>
-            <li v-if="item.text != 'Products'" class="v-breadcrumbs-divider">
-              /
-            </li>
-          </template>
-        </ul>
+        <v-breadcrumbs :items="items" />
       </v-app-bar>
       <v-sheet id="main" class="overflow-y-auto" max-height="100vh">
         <v-main style="position: relative">
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from "vue";
-import vuetify from "./plugins/vuetify";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
-const app = createApp(App);
-app.use(router);
-app.use(vuetify);
+Vue.config.productionTip = false;
 
-app.mount("#app");
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
